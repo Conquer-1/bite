f449caee7a961f95c0a2efde71c81c3d2a7895dd
Conquer-1 <2969776035@qq.com>
2021-01-12 22:10:32 +0800

    提交代码

---
+++ test_2021_1_12/test_2021_1_12/test.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//int Max(int a, int b)
+//{
+//	if (a > b)
+//		return a;
+//	else
+//		return b;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int max = 0;
+//	max = Max(a, b);
+//	printf("%d\n", max);
+//	return 0;
+//}
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	printf("%d\n", strlen("c:\test\121"));
+	return 0;
+}
