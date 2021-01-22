30f84e9a7f59e60875c05cd0eaad89ac60594c8c
Conquer-1 <2969776035@qq.com>
2021-01-22 22:51:17 +0800

    重新提交

--- test_2021_1_22/test_2021_1_22/test.c
+++ test_2021_1_22/test_2021_1_22/test.c
@@ -96,36 +96,36 @@
 //编写一个函数 reverse_string(char * string)（递归实现）
 //实现：将参数字符串中的字符反向排列。要求：不能使用C函数库中的字符串操作函数。
 //非递归实现
-//#include <stdio.h>
-//int my_strlen(char* p)
-//{
-//	if (*p != '\0')
-//	{
-//		return 1 + my_strlen(p + 1);
-//	}
-//	else
-//		return 0;
-//}
-//void reverse_string(char arr[])
-//{
-//	int left = 0;
-//	int right = my_strlen(arr) - 1;
-//	while (left < right)
-//	{
-//		int tmp = arr[left];
-//		arr[left] = arr[right];
-//		arr[right] = tmp;
-//		left++;
-//		right--;
-//	}
-//}
-//int main()
-//{
-//	char arr[] = "hello bit";
-//	reverse_string(arr);
-//	printf("%s\n", arr);
-//	return 0;
-//}
+#include <stdio.h>
+int my_strlen(char* p)
+{
+	if (*p != '\0')
+	{
+		return 1 + my_strlen(p + 1);
+	}
+	else
+		return 0;
+}
+void reverse_string(char arr[])
+{
+	int left = 0;
+	int right = my_strlen(arr) - 1;
+	while (left < right)
+	{
+		int tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+		left++;
+		right--;
+	}
+}
+int main()
+{
+	char arr[] = "hello";
+	reverse_string(arr);
+	printf("%s\n", arr);
+	return 0;
+}
 //递归实现
 //#include <stdio.h>
 //int my_strlen(char* p)
@@ -225,26 +225,26 @@
 //	return 0;
 //}
 //非递归实现
-#include <stdio.h>
-int fib(int n)
-{
-	int a = 1;
-	int b = 1;
-	int c = 1;
-	while (n > 2)
-	{
-		c = a + b;
-		a = b;
-		b = c;
-		n--;
-	}
-	return c;
-}
-int main()
-{
-	int n = 0;
-	scanf("%d", &n);
-	int ret = fib(n);
-	printf("ret = %d\n", ret);
-	return 0;
-}
+//#include <stdio.h>
+//int fib(int n)
+//{
+//	int a = 1;
+//	int b = 1;
+//	int c = 1;
+//	while (n > 2)
+//	{
+//		c = a + b;
+//		a = b;
+//		b = c;
+//		n--;
+//	}
+//	return c;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int ret = fib(n);
+//	printf("ret = %d\n", ret);
+//	return 0;
+//}
