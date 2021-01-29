87f304245b233f6ef2e3dbdca6cb5a1a8af04749
Conquer-1 <2969776035@qq.com>
2021-01-29 21:25:56 +0800

    提交代码

---
+++ test_2021_1_29/test_2021_1_29/test.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	if (strlen("abc") - strlen("abcdef") > 0)
+//	{
+//		printf("大于\n");
+//	}
+//	else
+//	{
+//		printf("小于\n");
+//	}
+//	return 0;
+//}
+////输出大于	strlen返回值unsigned int
+
+#include <stdio.h>
