51730aa5e2488392af78c82ba5b64ee082cd7e2f
Conquer-1 <2969776035@qq.com>
2021-01-29 09:13:29 +0800

    提交代码

--- test_2021_1_28/test_2021_1_28/test.c
+++ test_2021_1_28/test_2021_1_28/test.c
@@ -16,25 +16,32 @@
 //模拟实现库函数strlen
 //size_t strlen( const char *string );	size_t unsigned integer
 
-//#include <stdio.h>
-//#include <assert.h>
-//unsigned int my_strlen(const char *str)
-//{
-//	assert(str != NULL);
-//	unsigned int count = 0;
-//	while (*str++ != '\0')
-//	{
-//		count++;
-//	}
-//	return count;
-//}
-//int main()
-//{
-//	char str[] = "abcdefg";
-//	int count = my_strlen(str);
-//	printf("count = %d\n", count);
-//	return 0;
-//}
+#include <stdio.h>
+#include <assert.h>
+unsigned int my_strlen(const char *str)
+{
+	assert(str != NULL);
+	/*unsigned int count = 0;
+	char *start = str;
+	while (*start++ != '\0')
+	{
+		count++;
+	}
+	return count;*/
+	const char *start = str;
+	while (*str)
+	{
+		str++;
+	}
+	return str - start;
+}
+int main()
+{
+	char str[] = "abcdefg";
+	int count = my_strlen(str);
+	printf("count = %d\n", count);
+	return 0;
+}
 
 //模拟实现库函数strcpy
 //char *strcpy( char *strDestination, const char *strSource );
@@ -66,44 +73,44 @@
 //来调整该数组中数字的顺序使得数组中所有的奇数位于数组的前半部分，
 //所有偶数位于数组的后半部分。
 
-#include <stdio.h>
-void print(int arr[], int sz)
-{
-	int i = 0;
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-}
-void sort(int arr[], int sz)
-{
-	int left = 0;
-	int right = sz - 1;
-	while (left<right)
-	{
-		//从左边找偶数
-		while ((left<right) && (arr[left] % 2 == 1))
-		{
-			left++;
-		}
-		//从右边找奇数
-		while ((left<right) && (arr[right] % 2 == 0))
-		{
-			right--;
-		}
-		if (left < right)
-		{
-			int tmp = arr[left];
-			arr[left] = arr[right];
-			arr[right] = tmp;
-		}
-	}
-}
-int main()
-{
-	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	sort(arr,sz);
-	print(arr, sz);
-	return 0;
-}
+//#include <stdio.h>
+//void print(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//}
+//void sort(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left<right)
+//	{
+//		//从左边找偶数
+//		while ((left<right) && (arr[left] % 2 == 1))
+//		{
+//			left++;
+//		}
+//		//从右边找奇数
+//		while ((left<right) && (arr[right] % 2 == 0))
+//		{
+//			right--;
+//		}
+//		if (left < right)
+//		{
+//			int tmp = arr[left];
+//			arr[left] = arr[right];
+//			arr[right] = tmp;
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	sort(arr,sz);
+//	print(arr, sz);
+//	return 0;
+//}
